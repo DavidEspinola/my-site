c859d44c1451f295a3287f83943fe29c3574a9bb
DavidEspinola <DavidEspinola@users.noreply.github.com>
2021-04-30 21:03:52 +0000

    deploy: b4a8194cfc99fe99d3e6a0033c2abd29ac3ba76a

---
+++ _nuxt/static/1619816629/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-site\u002F_nuxt\u002Fstatic\u002F1619816629",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmy-site\u002F",assetsPath:"\u002Fmy-site\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b47d587a"}}}}(null));
