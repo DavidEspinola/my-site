e3df48eb0e660de20ec36cb8befef0632ba892d5
DavidEspinola <DavidEspinola@users.noreply.github.com>
2021-05-01 15:45:55 +0000

    deploy: 1d036a3852d250dace7d79909907b4f7d8e32059

--- _nuxt/static/1619816629/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-site\u002F_nuxt\u002Fstatic\u002F1619816629",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fmy-site\u002F",assetsPath:"\u002Fmy-site\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b47d587a"}}}}(null));
